7af87e0f0b22d33e12a4b8f0a8262eed0a803887
Alejandro Iglesias <ale.garciaiglesias@gmail.com>
2017-06-12 00:17:51 -0300

    Resolve excercise 4

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -40,6 +40,11 @@ export const Results = React.createClass({
           )}
         </div>
         <div className="management">
+          <button ref="restart"
+                  className="restart"
+                  onClick={this.props.restart}>
+            Restart
+          </button>
           <button ref="next"
                   className="next"
                   onClick={this.props.next}>
